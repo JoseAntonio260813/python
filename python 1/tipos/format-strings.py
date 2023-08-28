aff498a825d042ba93470d64c382a5a1e2ad8fbe
josé <jmarzer2608@g.educaand.es>
2023-08-28 20:22:44 +0200

    mas cosas

---
+++ python 1/tipos/format-strings.py
@@ -0,0 +1,4 @@
+nombre = "José"
+apellido = "Martínez"
+nombre_completo = f"{nombre} {apellido}"
+print(nombre_completo)
---
+++ python 1/tipos/metodos-strings.py
@@ -0,0 +1,9 @@
+animal = "	chanCHito feliz	"
+print(animal.upper())
+print(animal.lower())
+print(animal.strip().capitalize())
+print(animal.title())
+print(animal.strip())
+print(animal.rstrip())
+print(animal.lstrip())
+print(animal.find("cH"))
